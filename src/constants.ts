9d863fe37f5e4485727c53bf4a12e03b31c33ff4
Ziwen Hua <kinghua0629@outlook.com>
2026-05-17 20:57:49 +0200

    Add GISCUS constants for Giscus integration

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+import type { GiscusProps } from "@giscus/react";
+
+export const GISCUS: GiscusProps = {
+  repo: "kinghua0629/landing-v6",
+  repoId: "R_kgDOSgNGzg",
+  category: "General",
+  categoryId: "DIC_kwDOSgNGzs4C9Qyz",
+  mapping: "pathname",
+  reactionsEnabled: "0",
+  emitMetadata: "0",
+  inputPosition: "bottom",
+  lang: "en",
+  loading: "lazy",
+};
